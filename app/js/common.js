2002c32d6abea1fef118fe9544417794e80b2d5f
Evgen Skaskevich <skaskevich23@gmail.com>
2018-03-10 17:59:33 +0300

    fullsite

--- app/js/common.js
+++ app/js/common.js
@@ -1,51 +1,20 @@
+
 $(function() {
 
 
+// navigation
+
+// drop-down-menu
 $(".toggle-mnu").click(function() {
   $(this).toggleClass("on");
   $(".main-mnu").slideToggle();
   return false;
 });
-$(".toggle-mnu").click(function(e) {
-  e.preventDefault();
+$(".toggle-mnu").click(function() {
   $(".drop-down-menu").toggleClass('active');
 });
 
-$(".slick-prev").click(function(e) {
-   e.preventDefault();
-  $(".slick-prev").addClass('bg-white');
-  $(".slick-next").removeClass('bg-white');
-});
-$(".slick-next").click(function(e) {
-   e.preventDefault();
-  $(".slick-next").addClass('bg-white');
-  $(".slick-prev").removeClass('bg-white');
-
-});
-$("#slick-slide-control01").click(function(e) {
-   e.preventDefault();
-  $(".slick-next").addClass('bg-white');
-  $(".slick-prev").removeClass('bg-white');
-});
-$("#slick-slide-control01").click(function(e) {
-   e.preventDefault();
-  $(".slick-next").addClass('bg-white');
-  $(".slick-prev").removeClass('bg-white');
-});
-$("#slick-slide-control00").click(function(e) {
-   e.preventDefault();
-  $(".slick-prev").addClass('bg-white');
-  $(".slick-next").removeClass('bg-white');
-});
-$("#slick-slide-control00").click(function(e) {
-   e.preventDefault();
-  $(".slick-prev").addClass('bg-white');
-  $(".slick-next").removeClass('bg-white');
-});
-$(".close").click(function(e) {
-   e.preventDefault();
-  $(".pop-up-slider").addClass('hidden');
-});
+// sliders
 
 $('.slick-slider').slick({
 	dots: true,
@@ -92,7 +61,34 @@ $('.slick-slider-dignity').slick({
       }
     }]
 });
+$(".slick-prev").click(function() {
+  $(".slick-prev").addClass('bg-white');
+  $(".slick-next").removeClass('bg-white');
+});
+$(".slick-next").click(function() {
+  $(".slick-next").addClass('bg-white');
+  $(".slick-prev").removeClass('bg-white');
 
+});
+$("#slick-slide-control01").click(function() {
+  $(".slick-next").addClass('bg-white');
+  $(".slick-prev").removeClass('bg-white');
+});
+$("#slick-slide-control01").click(function() {
+  $(".slick-next").addClass('bg-white');
+  $(".slick-prev").removeClass('bg-white');
+});
+$("#slick-slide-control00").click(function() {
+  $(".slick-prev").addClass('bg-white');
+  $(".slick-next").removeClass('bg-white');
+});
+$("#slick-slide-control00").click(function() {
+  $(".slick-prev").addClass('bg-white');
+  $(".slick-next").removeClass('bg-white');
+});
+$(".close").click(function() {
+  $(".pop-up-slider").addClass('hidden');
+});
 
 $('.slider-comments').slick({
   dots: true,
@@ -132,5 +128,82 @@ $('.slider-gifts').slick({
     }]
   
 });
+// sliders end
 
 });
+
+
+
+
+// navigation
+;(function(){
+
+// var navigation = document.getElementById('nav_Mn');
+var selectTarget;
+var dropMenu = document.getElementsByClassName('drop-down-menu')[0];
+var dropMenuA = document.getElementsByClassName('toggle-mnu')[0];
+  
+  function modifClass(){
+      dropMenu.classList.remove('active');
+      dropMenuA.classList.remove('on');
+  };
+
+  function getCoords(elem) { // кроме IE8-
+  var box = elem.getBoundingClientRect();
+
+  return {
+    top: box.top + pageYOffset,
+    left: box.left + pageXOffset
+  };
+}
+
+  document.onclick = function(event){
+    var target = event.target;
+        switch(target.className.split(' ')[0]){
+        case 'nav_magforms':
+          window.scrollTo(0,0);
+          modifClass();
+          break;
+        case 'nav_catalog':
+          window.scrollTo(0,3670);
+          modifClass();
+          break;
+        case 'nav_rewards':
+          window.scrollTo(0,4800);
+          modifClass();
+          break;
+        case 'nav_reviews':
+          window.scrollTo(0,5600);
+          modifClass();
+          break;
+        case 'nav_delivery':
+          window.scrollTo(0,6300);
+          modifClass();
+          break;
+        case 'nav_basket':
+          window.scrollTo(0,9600);
+          modifClass();
+          break;
+        case 'nav_contacts':
+          window.scrollTo(0,10500);
+          modifClass();
+          break;
+      }   
+        if(selectTarget) {
+          selectTarget.classList.remove('active');
+        }
+        selectTarget = target;
+        selectTarget.classList.add('active');
+        
+        
+        console.log(getCoords(target));
+        
+        
+    
+    return false;
+  }
+
+
+  
+}());
+
